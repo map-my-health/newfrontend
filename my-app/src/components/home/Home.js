df7c6439c9684c3faba014e0a5ab3fda7b28ec57
Jennifer Hsiao <jchmai21@gmail.com>
2019-04-04 14:01:17 -0700

    changed home and json

--- my-app/src/components/home/Home.js
+++ my-app/src/components/home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Home.css';
 // import HomeContainer from '../../containers/events/HomeContainer';
+import Header from '../header/Header';
 
 function Home() {
   return (
     <section className={styles.Home}>
+    <Header />
     <p>This is home</p>
     </section>
   );
