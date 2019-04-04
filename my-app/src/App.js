1f00d5f14dc8cf20defb44dbe98cede0fa69a62b
Jennifer Hsiao <jchmai21@gmail.com>
2019-04-04 13:07:24 -0700

    Sidebar style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Sidebar from './Sidebar'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p> Helloworld!
-          </p>
-        </header>
+        <Sidebar />
       </div>
     );
   }
